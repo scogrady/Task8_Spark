9459ea5ebd54af4ac41749a56dadcbbbc8900c4f
Iris-lsy <iris.lsy.thy@gmail.com>
2015-02-17 16:36:14 -0500

    MapBean

--- src/controller/WebsiteAnalysisAction.java
+++ src/controller/WebsiteAnalysisAction.java
@@ -20,6 +20,7 @@ import org.scribe.model.Token;
 import org.scribe.model.Verb;
 import org.scribe.oauth.OAuthService;
 
+import databeans.MapBean;
 import databeans.TwitterBean;
 
 public class WebsiteAnalysisAction extends Action {
@@ -38,17 +39,18 @@ public class WebsiteAnalysisAction extends Action {
 
 		String resourceURL;
 		String searchParameters;
-		ArrayList<TwitterBean> result = new ArrayList<TwitterBean>();
-		HashMap<String, Integer> activeUser = new HashMap<String, Integer>();
-		HashMap<String, Integer> mostRetweet = new HashMap<String, Integer>();
-
+		searchParameters = "#love_adventure2";
+		resourceURL = "https://api.twitter.com/1.1/search/tweets.json";
+		
 		OAuthService service = (OAuthService) request.getSession()
 				.getAttribute("oauthService");
 		Token accessToken = (Token) request.getSession().getAttribute(
 				"accessToken");
+		
+		HashMap<String, Integer> activeUser = new HashMap<String, Integer>();
+		HashMap<String, Integer> mostRetweet = new HashMap<String, Integer>();
+		ArrayList<MapBean> mapList = new ArrayList<MapBean>();
 
-		searchParameters = "#love_adventure2";
-		resourceURL = "https://api.twitter.com/1.1/search/tweets.json";
 
 		try {
 
@@ -58,40 +60,49 @@ public class WebsiteAnalysisAction extends Action {
 			service.signRequest(accessToken, httpRequest);
 			Response response = httpRequest.send();
 
-			System.out.println(response.getBody());
-			System.out.println();
 
 			JSONObject jsonobject = new JSONObject(response.getBody());
 			JSONArray tweetArray = jsonobject.getJSONArray("statuses");
 
 			for (int i = 0; i < tweetArray.length(); i++) {
-				JSONObject tweet = tweetArray.getJSONObject(i);
-				TwitterBean tweetBean = new TwitterBean();
+				JSONObject tweet = tweetArray.getJSONObject(i);				
 
-				tweetBean.setId_str(tweet.getString("id_str"));
 				String id_str = tweet.getString("id_str");
 
+				JSONObject userObject = tweet.getJSONObject("user");
+				String user_id_str = userObject.getString("id_str");
+				String user_screen_name = userObject.getString("screen_name");
+
+				if (activeUser.containsKey(user_id_str)) {
+					Integer num = activeUser.get(user_id_str);
+					activeUser.replace(user_id_str, num, num + 1);
+				} else {
+					activeUser.put(user_id_str, 1);
+				}
+				
+				
+				
 				if (tweet.get("place") != org.json.JSONObject.NULL) {
 
 					JSONObject place = (JSONObject) tweet.get("place");
-
+					String placeName = place.getString("name");
 					JSONObject bounding_box = (JSONObject) place
 							.get("bounding_box");
 					JSONArray coordinateArray = (JSONArray) bounding_box
 							.get("coordinates");
 					JSONArray coordArray = (JSONArray) coordinateArray.get(0);
 					JSONArray coordinates = (JSONArray) coordArray.get(0);
-					System.out.println(coordinates.get(0).getClass() + "===");
 
-					tweetBean.setCoordinates(coordinates.getDouble(0),
-							coordinates.getDouble(1));
-					System.out.println(coordArray + "===");
-					System.out.println("==="
-							+ tweetBean.getCoordinates().getLongitude());
+					MapBean mapBean = new MapBean();
+					String mapDescrp = "";
+					mapBean.setLongitude(coordinates.getDouble(0));
+					mapBean.setLatitude(coordinates.getDouble(1));
+					mapBean.setDescrp(user_screen_name+" at "+placeName);
+					mapList.add(mapBean);					
 
 				}
 
-				tweetBean.setCreateTime(tweet.getString("created_at"));
+				String create_at = tweet.getString("created_at");
 
 				if (tweet.get("entities") != org.json.JSONObject.NULL) {
 					JSONObject entitiesObject = tweet.getJSONObject("entities");
@@ -102,37 +113,28 @@ public class WebsiteAnalysisAction extends Action {
 						JSONObject hashtag = hashtagsArray.getJSONObject(j);
 						hashtags[j] = hashtag.getString("text");
 					}
-					tweetBean.setEntities(hashtags);
 				}
 
-				tweetBean.setFavorite_count(tweet.getInt("favorite_count"));
-
-				if (tweet.get("in_reply_to_status_id_str") != org.json.JSONObject.NULL) {
-					tweetBean.setIn_reply_to_status_id_str((String) tweet
-							.get("in_reply_to_status_id_str"));
-				}
-
-				if (tweet.get("in_reply_to_user_id_str") != org.json.JSONObject.NULL) {
-					tweetBean.setIn_reply_to_user_id_str((String) tweet
-							.get("in_reply_to_user_id_str"));
-				}
-
-				tweetBean.setText(tweet.getString("text"));
-
-				tweetBean.setRetweet_count(tweet.getInt("retweet_count"));
 				int retweet = tweet.getInt("retweet_count");
 				mostRetweet.put(id_str, retweet);
 
-				JSONObject userObject = tweet.getJSONObject("user");
-				tweetBean.setUser_id_str(userObject.getString("id_str"));
-				String user_id_str = userObject.getString("id_str");
-				if (activeUser.containsKey(user_id_str)) {
-					Integer num = activeUser.get(user_id_str);
-					activeUser.replace(user_id_str, num, num + 1);
-				} else {
-					activeUser.put(user_id_str, 1);
+							
+				int favorite_count = tweet.getInt("favorite_count");
+				
+				String text = tweet.getString("text");
+
+
+				if (tweet.get("in_reply_to_status_id_str") != org.json.JSONObject.NULL) {
+					tweet
+							.get("in_reply_to_status_id_str");
 				}
-				result.add(tweetBean);
+
+				if (tweet.get("in_reply_to_user_id_str") != org.json.JSONObject.NULL) {
+					 tweet
+							.get("in_reply_to_user_id_str");
+				}
+
+				
 
 			}
 
@@ -156,6 +158,7 @@ public class WebsiteAnalysisAction extends Action {
 					}
 				});
 		for (String key : map.keySet()) {
+			
 			System.out.println(map.get(key));
 		}
 
