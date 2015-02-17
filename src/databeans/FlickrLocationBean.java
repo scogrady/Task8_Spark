4aa71b0b0fbd0c97fa7a6148201b6f40b68f7ccf
abhishekonline <abhishek4615@gmail.com>
2015-02-17 18:27:53 -0500

    FlickrLocation

---
+++ src/databeans/FlickrLocationBean.java
@@ -0,0 +1,25 @@
+package databeans;
+
+public class FlickrLocationBean {
+	private double x;
+	private double y;
+	private String url;
+	public double getX() {
+		return x;
+	}
+	public void setX(double x) {
+		this.x = x;
+	}
+	public double getY() {
+		return y;
+	}
+	public void setY(double y) {
+		this.y = y;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+}
